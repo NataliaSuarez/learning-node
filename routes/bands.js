137c6c571575590a5ea1ea0d53eef389d00accc8
natalia <natalia@camba.coop>
2020-06-07 19:26:43 -0300

    change bands to posts

--- routes/bands.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET bands page. */
-router.get('/', function(req, res, next) {
-    res.render('simple_response', { message: 'alsjflakjfalksf' });
-});
-
-module.exports = router;
