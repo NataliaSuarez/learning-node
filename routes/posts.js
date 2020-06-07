b449da7ef61ace974aa0b8d58b805ecdd64ad3d0
natalia <natalia@camba.coop>
2020-06-07 20:15:33 -0300

    add form with new add route and css

--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
     res.render('posts', { message: 'posts display here' });
 });
 
+router.post('/add', function(req, res){
+    console.log(req.body);
+    res.send('okiii <3');
+});
+
 module.exports = router;
