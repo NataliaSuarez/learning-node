b4dca2ef014e88f26627e683e3cd433abc2fafa9
natalia <natalia@camba.coop>
2020-06-07 12:28:44 -0300

    added mongoose

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bandsRouter = require('./routes/bands');
 
 var app = express();
+
+mongoose.connect(
+  'mongodb://localhost/app420',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(db => console.log('Database connected! ;)'))
+  .catch(err => console.log(err));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
