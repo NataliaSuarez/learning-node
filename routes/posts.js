029c3324179f0e7d5e302ee93f41e5ea138386cb
natalia <natalia@camba.coop>
2020-06-08 19:40:51 -0300

    updated get and post for posts

--- routes/posts.js
+++ routes/posts.js
@@ -4,14 +4,16 @@ var router = express.Router();
 const Post = require('../models/post');
 
 /* GET posts page. */
-router.get('/', function(req, res, next) {
-    res.render('posts', { message: 'posts display here' });
+router.get('/', async (req, res, next) => {
+    const posts = await Post.find();
+    console.log(posts);
+    res.render('posts', { posts: posts });
 });
 
 router.post('/add', async (req, res) => {
     const post = new Post(req.body);
     await post.save();
-    res.send('okiii <3');
+    res.redirect('/posts');
 });
 
 module.exports = router;
