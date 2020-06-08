390ba01d232c7611583a41871a2c86ec20fcdfcb
natalia <natalia@camba.coop>
2020-06-08 19:10:01 -0300

    create post

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const Post = require('../models/post');
+
 /* GET posts page. */
 router.get('/', function(req, res, next) {
     res.render('posts', { message: 'posts display here' });
 });
 
-router.post('/add', function(req, res){
-    console.log(req.body);
+router.post('/add', async (req, res) => {
+    const post = new Post(req.body);
+    await post.save();
     res.send('okiii <3');
 });
 
