11ae2cb7446a6abb2d56c3d6867d27a6b3267aa8
natalia <natalia@camba.coop>
2020-06-08 21:11:18 -0300

    add delete route

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const Post = require('../models/post');
 
 /* GET posts page. */
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
     const posts = await Post.find();
     res.render('posts', { posts: posts });
 });
@@ -12,6 +12,12 @@ router.get('/', async (req, res, next) => {
 router.post('/add', async (req, res) => {
     const post = new Post(req.body);
     await post.save();
+    res.redirect('/posts');
+});
+
+router.get('/delete/:id', async(req, res) => {
+    const { id } = req.params;
+    await Post.remove({_id:id});
     res.redirect('/posts');
 });
 
